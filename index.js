747aff9b90c2adf27ca83b9eb073abb7915047cb
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-08 17:50:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
-function loadIssue() {
+function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0;i<issues.length;i++) {
-    var result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
 }
 
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
